92c08650a0a2a8447a52a01ab3efe097b26fa0f8
ad1anastia <anastasiaserokurova@gmail.com>
2025-08-21 14:45:57 +0700

    Создание файла Constants.swift

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey  = "wszUig8pYoh4rsY0OONc88yxixfN-aFucjtIYAv3DC0"
+    static let secretKey = "MtAh129S_RNuX4wW2cTvAdeQG8NP2zrVi5iMZqARdBo"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
